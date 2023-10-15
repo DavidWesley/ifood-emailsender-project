5913e5aa440ff9224e5b6d4def91956548927ae7
David Wesley <contato.dwesley@outlook.com>
2023-10-15 10:32:53 -0300

    refactor(db/validators): Improve isValidName validation

--- src/lib/db/validators/index.ts
+++ src/lib/db/validators/index.ts
@@ -1,5 +1,5 @@
 export const Validators = Object.freeze({
-    isValidName: (name: string): boolean => name.length > 3 && name.length <= 100,
+    isValidName: (name: string): boolean => name.length >= 1 && name.length <= 100,
     isUUID: (id: string): boolean => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(id),
     isEmail: (email: string): boolean => /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i.test(email),
     isDateTimeString: (date: string): boolean => /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/.test(date),
