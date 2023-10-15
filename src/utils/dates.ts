222bc311dcdce5686aaeebcd6d719c47346ee7b5
David Wesley <contato.dwesley@outlook.com>
2023-10-15 12:12:25 -0300

    feat(utils): Add isFirstDateInPreviousMonth function

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,19 @@
 export const getWeekDayFullName = (date: Date, locale: string): string => {
     return date.toLocaleDateString(locale ?? "pt-BR", { weekday: "long" })
 }
+
+export const isFirstDateInPreviousMonth = (startDate: string, endDate: string): boolean => {
+    const start = new Date(startDate)
+    const end = new Date(endDate)
+
+    const startMonth = start.getUTCMonth()
+    const startYear = start.getUTCFullYear()
+
+    const endMonth = end.getUTCMonth()
+    const endYear = end.getUTCFullYear()
+
+    return (
+        (endYear - startYear === 1 && Math.abs(endMonth - startMonth) === 11) ||
+        (startYear === endYear && endMonth - startMonth === 1)
+    )
+}
