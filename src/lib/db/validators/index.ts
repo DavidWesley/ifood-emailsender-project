d1fdf574c0dfc343cddbfb2369643e7133f3138b
David Wesley <contato.dwesley@outlook.com>
2023-10-15 10:30:00 -0300

    fix(db/validators): fix isDateTimeString regex

--- src/lib/db/validators/index.ts
+++ src/lib/db/validators/index.ts
@@ -2,6 +2,6 @@ export const Validators = Object.freeze({
     isValidName: (name: string): boolean => name.length > 3 && name.length <= 100,
     isUUID: (id: string): boolean => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(id),
     isEmail: (email: string): boolean => /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i.test(email),
-    isDateTimeString: (date: string): boolean => /^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{${args.precision}}Z$/.test(date),
+    isDateTimeString: (date: string): boolean => /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/.test(date),
     isBoolean: (value: string): boolean => value === "true" || value === "false",
 })
